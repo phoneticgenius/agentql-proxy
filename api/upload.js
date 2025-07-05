6ae65fbcf36490e3065b156e794e8920bdf5d6e1
phoneticgenius <phoneticgenius@gmail.com>
2025-07-05 11:17:17 +0200

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
       contentType,
     });
 
-    const graphqlBody = {
+    const graphqlQuery = {
   query: `query {
     job_posting {
       job_title
