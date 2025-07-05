577ee42e4cf50f0cb9df6293121d50373006ceb7
phoneticgenius <phoneticgenius@gmail.com>
2025-07-05 11:40:49 +0200

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 async function uploadFileManualMultipart(filePath, fileName, queryPrompt, apiKey) {
   const boundary = '------------------------abcdef1234567890';
